b490ff6a5c68a0e71fd947b7cd99e64a2436fe17
mondriy <58882902+mondriy@users.noreply.github.com>
2022-07-09 14:02:35 +0300

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import './App.css';
-
-interface IMath {
-  asf: string;
-}
+import 'assets/styles/Rules.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className='App'></div>;
 }
 
 export default App;
